d2b8c7d90fcaff9a0ad589a3616002a6bc9ce085
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-13 21:40:06 -0300

    add - verificação de erro ao preparar a consulta no excluir.php

--- public/excluir.php
+++ public/excluir.php
@@ -3,6 +3,9 @@ include "../infra/conexao.php";
 $id = $_GET["id"];
 $sql = "DELETE FROM livros WHERE id=?";
 $stmt = mysqli_prepare($conexao, $sql);
+if (!$stmt) {
+    die("Erro ao preparar a consulta.");
+}
 mysqli_stmt_bind_param($stmt, "i", $id);
 mysqli_stmt_execute($stmt);
 header("Location: ../index.php");
